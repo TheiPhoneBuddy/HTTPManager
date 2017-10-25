253200ab5f64f0832a04903529992f62665fbdcb
TheiPhoneBuddy <TheiPhoneBuddy@gmail.com>
2017-10-25 13:21:23 -0700

    Add files via upload

---
+++ Http Manager/HTTPManager.framework/Headers/HTTPManager.h
@@ -0,0 +1,17 @@
+//
+//  HTTPManager.h
+//  HTTPManager
+//
+//  Created by Francis Chan on 6/3/17.
+//  Copyright © 2017 TheiPhoneBuddy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HTTPManager.
+FOUNDATION_EXPORT double HTTPManagerVersionNumber;
+
+//! Project version string for HTTPManager.
+FOUNDATION_EXPORT const unsigned char HTTPManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HTTPManager/PublicHeader.h>
